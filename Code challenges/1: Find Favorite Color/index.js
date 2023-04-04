9f5154f9efcf77df5f9deede57f66de234076e82
rayniel95 <rainyel.ramos@gmail.com>
2023-04-04 18:16:00 -0400

    init

---
+++ Code challenges/1: Find Favorite Color/index.js
@@ -0,0 +1,18 @@
+const { sha256 } = require("ethereum-cryptography/sha256");
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
+
+// the possible colors that the hash could represent
+const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
+
+// given a hash, return the color that created the hash
+function findColor(hash) {
+    for (let color of COLORS){
+        const colorInBytes = utf8ToBytes(color)
+        const colorHash = sha256(colorInBytes)
+        if (toHex(hash) === toHex(colorHash)) { 
+            return color
+        }
+    }
+}
+
+module.exports = findColor;
