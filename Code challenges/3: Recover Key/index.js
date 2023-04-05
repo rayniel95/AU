b16ea105fbd0e3b85ac0ba476dd34b10fe83af29
rayniel95 <rainyel.ramos@gmail.com>
2023-04-04 22:29:17 -0400

    add recover public key

--- Code challenges/3: Recover Key/index.js
+++ Code challenges/3: Recover Key/index.js
@@ -1,10 +1,8 @@
 const secp = require("ethereum-cryptography/secp256k1");
-const hashMessage = require('../1: Hash Message/index');
+const hashMessage = require("../1: Hash Message/index");
 
-const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
-
-async function signMessage(msg) {
-    return await secp.sign(hashMessage(msg), PRIVATE_KEY, {recovered: true})
+async function recoverKey(message, signature, recoveryBit) {
+    return secp.recoverPublicKey(hashMessage(message), signature, recoveryBit)
 }
 
-module.exports = signMessage;
+module.exports = recoverKey;
